32c7bc389e1b047ede240f7696f8a76935f638d4
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2023-08-29 17:45:45 +0200

    -

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require("body-parser")
 require('dotenv').config()
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
+app.post("api/users", (req, res) => {
+  console.log(req.body)
+})
 
 
 
